c56c8245ab3a799c0d3cbb8d816e3187585fbcc0
БарашенковИТ <@101-09>
2022-10-17 16:15:01 +0300

    ass

--- Assembler/Source.c
+++ Assembler/Source.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <locale.h>
+#define n 5
 
 int main(void)
 {
@@ -12,8 +13,23 @@ int main(void)
 	ost(12, 5);
 	sravnC(2, 5);
 	CompareThree(3, 2, 5);
+	Swap(2, 4, 5);
 	return 0;
+
+	int* arr = calloc(n, sizeof(int));
+	for (size_t i = 0; i < n; i++)
+	{
+		arr[i] = i + 1;
+	}
+	arr = Swap(arr, 0, 4);
+	for (size_t i = 0; i < n; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	putchar();
 }
+
+
 
 int summ(int a, int b) 
 {
@@ -235,4 +251,23 @@ int sravnC3(int a, int b, int c)
 	return d;
 }
 
+int* Swap(int* arr, int n, int n2 )
+{
+	int a = arr[n]; 
+	int b = arr[n2];
+	__asm 
+	{
+		mov eax, a
+		mov ebx, b
+		mov ecx, ebx
+		mov ebx, eax
+		mov eax, ecx
+		mov a, eax
+		mov b, ebx
+	}
+	arr[n] = a;
+	arr[n2] = b;
+	return arr;
+	printf("Ответ: %d\n", arr);
+}
 
